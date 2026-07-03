53784452cdd237edfca97aea7cb91340433d5978
ayushimehta020 <ayushimehta020@gmail.com>
2026-07-03 16:57:07 +0530

    setup RabbitMQ

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,19 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connected to RabbitMQ!");
+
+  process.on("SIGINT", async () => {
+    console.log("Shutting down...");
+
+    await conn.close();
+    console.log("RabbitMQ connection closed")
+
+    process.exit(0);
+  });
 }
 
 main().catch((err) => {
