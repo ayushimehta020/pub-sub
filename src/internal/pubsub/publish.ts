e12f93c9c7e77a6080556edb089e3cc450df2bf9
ayushimehta020 <ayushimehta020@gmail.com>
2026-07-07 11:42:11 +0530

    data serialization using msgpack

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import type { ConfirmChannel } from "amqplib";
+import msgpack from "@msgpack/msgpack";
 
 export function publishJSON<T>(
   ch: ConfirmChannel,
@@ -9,5 +10,17 @@ export function publishJSON<T>(
   const buffer = Buffer.from(JSON.stringify(value));
   ch.publish(exchange, routingKey, buffer, {
     contentType: "application/json",
+  });
+}
+
+export async function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const buffer = Buffer.from(msgpack.encode(value));
+  ch.publish(exchange, routingKey, buffer, {
+    contentType: "application/x-msgpack",
   });
 }
